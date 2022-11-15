2da1195c4ddf36b9daf41694c82acf446663e7b3
Meisam.naderi@samspar.no <Meisam.naderi@samspar.no>
2022-11-15 11:39:48 +0000

    add 2

---
+++ MEN_First_Table.sql
@@ -0,0 +1,26 @@
+{{ config(materialized='table') }}
+
+select *  from MEN_TEST
+;
+
+
+
+
+
+
+
+
+
+/*
+{{ config(materialized='table') }}
+
+with source_data as (
+
+    select 1 as id
+    union all
+    select null as id
+
+)
+
+select *
+from source_data;
