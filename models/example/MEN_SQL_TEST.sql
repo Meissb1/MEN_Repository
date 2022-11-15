01f2242e26b721d3e518ff6e1f69dfa0096a5b15
Meisam.naderi@samspar.no <Meisam.naderi@samspar.no>
2022-11-15 12:53:16 +0000

    amy_c_dbt_workshop

--- models/example/MEN_SQL_TEST.sql
+++ models/example/MEN_SQL_TEST.sql
@@ -2,6 +2,6 @@
 {{ config(materialized='table') }}
 
 select *
-from 'PC_DBT_DB.DBT_MEISSB1.MEN_TEST'
+from PC_DBT_DB.DBT_MEISSB1.MEN_TEST
 --from {{ ref('PC_DBT_DB.DBT_MEISSB1.MEN_TEST') }}
 where DatasetId = 'USINDSSP2020'
